7be0878a44c728db716752f8f7574a286d3526ab
Kauã Veríssimo <187907796+V341551M0@users.noreply.github.com>
2026-09-12 12:44:47 -0500

    style: format code with prettier

--- frontend/routes/AppTY.js
+++ frontend/routes/AppTY.js
@@ -1,8 +1,8 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 
-import Home from "../frontend/src/pages/home/home";
-import Login from "./pages/Login"; /*<-- Corrigido Futuramente*/
-import Dashboard from "./pages/Dashboard"; /*<-- Corrigido Futuramente*/
+import Home from '../frontend/src/pages/home/home';
+import Login from './pages/Login'; /*<-- Corrigido Futuramente*/
+import Dashboard from './pages/Dashboard'; /*<-- Corrigido Futuramente*/
 
 export default function AppRoutes() {
     return (
--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -1,5 +1,5 @@
-fetch("components/navbar.html")
-    .then(response => response.text())
-    .then(html => {
-        document.getElementById("navbar").innerHTML = html;
+fetch('components/navbar.html')
+    .then((response) => response.text())
+    .then((html) => {
+        document.getElementById('navbar').innerHTML = html;
     });
