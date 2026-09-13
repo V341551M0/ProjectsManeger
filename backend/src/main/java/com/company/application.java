2623de484cbcc479f11b83adfb91799305d7b508
Kauã Veríssimo <187907796+V341551M0@users.noreply.github.com>
2026-09-13 15:26:05 -0500

    feat: add database schema migrations

--- backend/src/main/java/com/company/application.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class application {
-    public static void main(String[] args){
-        
-    }
-}
---
+++ backend/src/main/java/com/projectsmaneger/ProjectsManegerApplication.java
@@ -0,0 +1,12 @@
+package com.projectsmaneger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsManegerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectsManegerApplication.class, args);
+    }
+}
