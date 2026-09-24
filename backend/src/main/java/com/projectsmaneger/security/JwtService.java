79fbbfdd623301add09cfc1a562257509e20393a
Kauã Veríssimo <187907796+V341551M0@users.noreply.github.com>
2026-09-24 13:49:09 -0500

    feat: spring boot configuration

--- backend/src/main/java/com/projectsmaneger/security/JwtService.java
+++ backend/src/main/java/com/projectsmaneger/security/JwtService.java
@@ -6,36 +6,72 @@ import java.util.Date;
 import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
-    
+
     private final SecretKey secretKey;
 
     public JwtService(
-        @Value("${jwt.secret}") String secret
+            @Value("${jwt.secret}") String secret
     ) {
         this.secretKey = Keys.hmacShaKeyFor(
-            secret.getBytes(StandardCharsets.UTF_8)
+                secret.getBytes(StandardCharsets.UTF_8)
         );
     }
 
     public String generateToken(String username) {
-        
+
         Date now = new Date();
-        Date expiration = new Date (
-            now.getTime() + 1000L * 60 * 60
+
+        Date expiration = new Date(
+                now.getTime() + 1000L * 60 * 60
         );
 
         return Jwts.builder()
-            .subject(username)
-            .issuedAt(now)
-            .expiration(expiration)
-            .signWith(secretKey)
-            .compact();
+                .subject(username)
+                .issuedAt(now)
+                .expiration(expiration)
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+
+        return extractAllClaims(token)
+                .getSubject();
+    }
+
+    public boolean isTokenValid(
+            String token,
+            UserDetails userDetails
+    ) {
+
+        String username = extractUsername(token);
+
+        return username.equals(userDetails.getUsername())
+                && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+
+        return extractAllClaims(token)
+                .getExpiration()
+                .before(new Date());
+    }
+
+    private Claims extractAllClaims(String token) {
+
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 }
--- backend/src/test/java/com/projectsmaneger/security/JwtServiceTest.java
+++ backend/src/test/java/com/projectsmaneger/security/JwtServiceTest.java
@@ -3,24 +3,75 @@ package com.projectsmaneger.security;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 
 class JwtServiceTest {
 
     @Test
     void shouldGenerateValidToken() {
 
-        String secret = "ajbsajBAKSnkjsBJABSjkcbaBKJASBKJABSKKJKJ";
+        String secret =
+                "ajbsajBAKSnkjsBJABSjkcbaBKJASBKJABSKKJKJ";
 
-        JwtService jwtService = new JwtService(secret);
+        JwtService jwtService =
+                new JwtService(secret);
 
-        String token = jwtService.generateToken("verissimo");
+        String token =
+                jwtService.generateToken("verissimo");
 
         assertNotNull(token);
         assertFalse(token.isBlank());
 
-        String[] parts = token.split("\\.");
+        String[] parts =
+                token.split("\\.");
 
         assertEquals(3, parts.length);
+    }
+
+    @Test
+    void shouldExtractUsernameFromToken() {
+
+        String secret =
+                "ajbsajBAKSnkjsBJABSjkcbaBKJASBKJABSKKJKJ";
+
+        JwtService jwtService =
+                new JwtService(secret);
+
+        String token =
+                jwtService.generateToken("verissimo");
+
+        String username =
+                jwtService.extractUsername(token);
+
+        assertEquals("verissimo", username);
+    }
+
+    @Test
+    void shouldValidateToken() {
+
+        String secret =
+                "ajbsajBAKSnkjsBJABSjkcbaBKJASBKJABSKKJKJ";
+
+        JwtService jwtService =
+                new JwtService(secret);
+
+        String token =
+                jwtService.generateToken("verissimo");
+
+        UserDetails userDetails =
+                User.withUsername("verissimo")
+                        .password("senha")
+                        .roles("USER")
+                        .build();
+
+        assertTrue(
+                jwtService.isTokenValid(
+                        token,
+                        userDetails
+                )
+        );
     }
 }
