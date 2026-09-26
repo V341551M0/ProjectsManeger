e4e278c55a3ad20b55f0890f7dd9abb64ce28d2d
Kauã Veríssimo <187907796+V341551M0@users.noreply.github.com>
2026-09-26 01:20:19 -0500

    style: frontend architecture

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -7,6 +7,25 @@ function Navbar () {
     function toggleMenu() {
         setMenuOpen(!menuOpen)
     }
+
+    return (
+        <header className="Navbar">
+            <Link to="/" className="navbar__logo">
+                Projects Maneger
+            </Link>
+
+            <nav className="Navbar_links" aria-label="Navegaçao Principal">
+                <Link to="/">Pesquisar</Link>
+                <Link to="">Gerir Todos os Projetos</Link>
+                <Link to="">Filtros</Link> {/* <-- Corrigido Futuramente */}
+                <Link to="">Recentes</Link> 
+                <Link to="">Pessoas</Link>
+                <Link to="">Ordenar</Link> {/* <-- Corrigido Futuramente */}
+                <Link to="">Ocultar</Link> {/* <-- Corrigido Futuramente */}
+                <Link to=""></Link> {/* <-- Corrigido Futuramente */}
+            </nav>
+        </header>
+    )
 }
 
 export default 'Navbar'
